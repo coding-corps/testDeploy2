e1b3836abe8499be2fceb608029c8882f00c09fe
kevin <kevin@paay.co>
2024-05-11 16:53:19 -0400

    test

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/testDeploy2/' // Change 'your-repo-name' to your GitHub repository name\
 })
